e028bfacdaeb207f12c2316529c67cfaf9e11bfc
Eduu <100446318+ppyLEK@users.noreply.github.com>
2023-03-30 11:50:01 +0300

    Add scripts

---
+++ Assets/Universal/Gamestate/PlayerSceneData.cs
@@ -0,0 +1,73 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public namespace ppyLEK.Util.Save
+{
+
+
+public static class PlayerSceneData
+{
+        public static void SaveScene(Scene scene)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string path = Application.persistentDataPath + "/data.scene";
+            FileStream stream = new FileStream(path, FileMode.Create);
+
+            PlayerData data = new PlayerData(player);
+
+            formatter.Serialize(stream, data);
+            stream.Close();
+        }
+        public static void SavePlayer(Player player)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string path = Application.persistentDataPath + "/data.player";
+            FileStream stream = new FileStream(path, FileMode.Create);
+
+            PlayerData data = new PlayerData(player);
+
+            formatter.Serialize(stream, data);
+            stream.Close();
+        }
+
+        public static PlayerData LoadPlayer()
+        {
+            string path = Application.persistentDataPath + "/data.player";
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path, FileMode.Open);
+
+                PlayerData data = formatter.Deserialize(stream) as PlayerData;
+                stream.Close();
+
+                return data;
+            }
+            else
+            {
+                Debug.LogError("Save file not found in " + path);
+                return null;
+            }
+        }
+        public static SceneData LoadScene()
+        {
+            string path = Application.persistentDataPath + "/data.scene";
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path, FileMode.Open);
+
+                PlayerData data = formatter.Deserialize(stream) as PlayerData;
+                stream.Close();
+
+                return data;
+            }
+            else
+            {
+                Debug.LogError("Save file not found in " + path);
+                return null;
+            }
+        }
+    }
+}
